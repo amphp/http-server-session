53fdd1ca86d40f7a867b6cc4798fa4275aec813d
Niklas Keller <me@kelunik.com>
2015-05-31 03:40:16 +0200

    Implement Redis driver

---
+++ lib/Session/Redis.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Aerys\Session;
+
+use Amp\Promise;
+use Amp\Reactor;
+use Amp\Redis\Client;
+use Amp\Redis\Mutex;
+use Amp\Success;
+use function Amp\pipe;
+
+class Redis implements Driver {
+    private $reactor;
+    private $client;
+    private $mutex;
+    private $locks;
+
+    public function __construct (Client $client, Mutex $mutex, Reactor $reactor) {
+        $this->client = $client;
+        $this->mutex = $mutex;
+        $this->reactor = $reactor;
+        $this->locks = [];
+
+        $this->reactor->repeat(function () {
+            foreach ($this->locks as $id => $token) {
+                $this->mutex->renew($id, $token, 1000);
+            }
+        }, 500);
+    }
+
+    /**
+     * Creates a lock and reads the current session data
+     * @return \Amp\Promise resolving to an array with current session data
+     */
+    public function open (string $id): Promise {
+        $token = uniqid("", true);
+
+        return pipe($this->mutex->lock($id, $token, 1000, 1), function () use ($id, $token) {
+            $this->locks[$id] = $token;
+            return $this->read($id);
+        });
+    }
+
+    /**
+     * Saves and unlocks a session
+     * @param array $data to store
+     * @return \Amp\Promise resolving after success
+     */
+    public function save (string $id, array $data): Promise {
+        if (empty($data)) {
+            return pipe($this->client->del("sess:" . $id), function () use ($id) {
+                return $this->unlock($id);
+            });
+        } else {
+            return pipe($this->client->set("sess:" . $id, serialize($data)), function () use ($id) {
+                return $this->unlock($id);
+            });
+        }
+    }
+
+    /**
+     * Regenerates a session id
+     * @return \Amp\Promise resolving after success
+     */
+    public function regenerate (string $oldId, string $newId): Promise {
+        $token = $this->locks[$oldId] ?? "";
+
+        return pipe($this->mutex->lock($newId, $token, 1000, 1), function () use ($oldId, $token) {
+            return $this->mutex->unlock($oldId, $token);
+        });
+    }
+
+    /**
+     * Reloads the session contents
+     * @return \Amp\Promise resolving to an array with current session data
+     */
+    public function read (string $id): Promise {
+        return pipe($this->client->get("sess:" . $id), function ($result) {
+            if ($result) {
+                return unserialize($result);
+            } else {
+                return [];
+            }
+        });
+    }
+
+    /**
+     * Unlocks the session, reloads data without saving
+     * @return \Amp\Promise resolving to an array with current session data
+     */
+    public function unlock (string $id): Promise {
+        $token = $this->locks[$id] ?? "";
+
+        if ($token) {
+            return pipe($this->mutex->unlock($id, $token), function () use ($id) {
+                unset($this->locks[$id]);
+            });
+        }
+
+        return new Success;
+    }
+}
