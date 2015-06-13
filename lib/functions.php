f36e1910bd245a52e79a919131c26a135abd243a
Niklas Keller <me@kelunik.com>
2015-06-13 17:28:29 +0200

    Make middleware implementation compatible

--- lib/functions.php
+++ lib/functions.php
@@ -16,7 +16,7 @@ function session(array $config = []) {
             $this->config = $config;
         }
 
-        public function do(InternalRequest $request) {
+        public function do(InternalRequest $request, Options $options) {
             $request->locals["aerys.session.config"] = $this->config;
 
             $message = "";
